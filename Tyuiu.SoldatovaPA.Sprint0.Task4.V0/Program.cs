d72f4523106d923954352c5bd5431bbb5453b435
Полина Солдатова <clevnay@mail.ru>
2025-09-14 19:45:23 +0500

    Task3

---
+++ Tyuiu.SoldatovaPA.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.SoldatovaPA.Sprint0.Task4.V0.Lib;
+
+namespace Tyuiu.SoldatovaPA.Sprint0.Task4.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Пример линейной структуры
+            //Вызов метода сложения Addition
+            Console.WriteLine(DataService.Addition(1, 5));
+            //Вызов метода сложения Addition
+            Console.WriteLine(DataService.Subtraction(15, 5));
+            //Вызов метода сложения Addition
+            Console.WriteLine(DataService.Multiplication(10, 10));
+            //Вызов метода сложения Addition
+            Console.WriteLine(DataService.Division(5, 5));
+
+            Console.ReadKey();
+        }
+    }
+}
